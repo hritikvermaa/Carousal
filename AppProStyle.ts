d7afcdce0298e3ff43330f7eb1209318ea982323
Ritik kumar verma <hritikverma2575@gmail.com>
2024-02-24 19:41:54 +0530

    First commit

---
+++ AppProStyle.ts
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native'
+
+const AppProStyle= StyleSheet.create(
+    {
+        container:{
+            display:'flex',
+            alignItems: 'center',
+            justifyContent:'center'
+          
+        },
+        whiteText:{
+            color: '#FFF'
+        },
+        darkText:{
+            color: '#000'
+        },
+    }
+)
+
+export default AppProStyle;
